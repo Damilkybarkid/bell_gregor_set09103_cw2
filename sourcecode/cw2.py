29358922b255573ed787075d465eeba126e054d0
Gregor Bell <gregorbell@virginmedia.com>
2018-11-25 07:45:01 +0000

    Got the team form working. Created a screen that displays teams created by the current user. Added a comments secion to the team details page. Created admin accounts. Improved the css. Added a pokemon upload feature. Restricted the admin registration and pokemon upload features to admins only. Added custom error pages. Added a page of all teams that have been created.

--- sourcecode/cw2.py
+++ sourcecode/cw2.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, json, url_for, request, redirect, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, IntegerField, HiddenField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Supersecretnevertellno-one'
@@ -21,6 +22,7 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(15), unique=True)
 	email = db.Column(db.String(50), unique=True)
 	password = db.Column(db.String(80))
+	administrator = db.Column(db.Boolean, default=False)
 
 class Pokemon(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -36,12 +38,19 @@ class Team(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50), unique=True)
 	author = db.Column(db.String(15))
-	pokemon1 = db.Column(db.String(30))
-	pokemon2 = db.Column(db.String(30))
-	pokemon3 = db.Column(db.String(30))
-	pokemon4 = db.Column(db.String(30))
-	pokemon5 = db.Column(db.String(30))
-	pokemon6 = db.Column(db.String(30))
+	pokemon1name = db.Column(db.String(30))
+	pokemon2name = db.Column(db.String(30))
+	pokemon3name = db.Column(db.String(30))
+	pokemon4name = db.Column(db.String(30))
+	pokemon5name = db.Column(db.String(30))
+	pokemon6name = db.Column(db.String(30))
+
+class Comment(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(140))
+	author = db.Column(db.String(15))
+	timestamp = db.Column(db.DateTime(), default=datetime.utcnow, index=True)
+	teamName = db.Column(db.String(30))
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -57,6 +66,12 @@ class RegisterForm(FlaskForm):
 	email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
 	password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
 
+class RegisterAdminForm(FlaskForm):
+	username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+	email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+	password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+	administrator = BooleanField('Is Administrator?')
+
 class PokemonForm(FlaskForm):
 	id = IntegerField('ID', validators=[InputRequired()])
 	name = StringField('Name', validators=[InputRequired(), Length(min=1, max=30)])
@@ -68,23 +83,91 @@ class PokemonForm(FlaskForm):
 	pokedexEntry = StringField('Pokedex Entry', validators=[InputRequired(), Length(min=1, max=1000)])
 	
 class TeamForm(FlaskForm):
-	name = StringField('Name', validators=[InputRequired(), Length(min=1, max=50)])
+	name = StringField('Team Name', validators=[InputRequired(), Length(min=1, max=50)])
+	pokemon1name = HiddenField('Pokemon 1', validators=[InputRequired(), Length(min=1, max=30)])
+	pokemon2name = HiddenField('Pokemon 2', validators=[InputRequired(), Length(min=1, max=30)])
+	pokemon3name = HiddenField('Pokemon 3', validators=[InputRequired(), Length(min=1, max=30)])
+	pokemon4name = HiddenField('Pokemon 4', validators=[InputRequired(), Length(min=1, max=30)])
+	pokemon5name = HiddenField('Pokemon 5', validators=[InputRequired(), Length(min=1, max=30)])
+	pokemon6name = HiddenField('Pokemon 6', validators=[InputRequired(), Length(min=1, max=30)])
+
+class CommentForm(FlaskForm):
+	text = StringField('Text', validators=[InputRequired(), Length(min=1, max=140)])
 
 #with open('static/data/pokemon.json') as pokemon_json:
 #	pokemon = json.load(pokemon_json)
 
 pokemon = db.session.query(Pokemon).all()
-
+ 
 @app.route('/')
 def root():
 	return redirect(url_for('home'))
 
-@app.route('/home/')
+@app.route('/home/', methods=['GET', 'POST'])
 #@login_required
 def home():
 	form = TeamForm()
+	
+	if form.validate_on_submit():
+		if current_user.is_authenticated:
+			newTeam = Team(name=form.name.data, author=current_user.username, pokemon1name=form.pokemon1name.data, pokemon2name=form.pokemon2name.data, pokemon3name=form.pokemon3name.data, pokemon4name=form.pokemon4name.data, pokemon5name=form.pokemon5name.data, pokemon6name=form.pokemon6name.data)
+			db.session.add(newTeam)
+			db.session.commit()
+	
+			url = 'http://set09103.napier.ac.uk:9112/teams/' + form.name.data + '/'	
+			return redirect(url)
+		else:
+			return redirect(url_for('login'))
 
-	return render_template('home.html', pokemon=pokemon, name=current_user.username, form=form), 200
+	return render_template('home.html', pokemon=pokemon, form=form), 200
+
+@app.route('/users/<user_name>/')
+@login_required
+def userTeams(user_name):
+	teams = db.session.query(Team).all()
+	teamNames = []
+
+	if user_name == current_user.username:
+		for team in teams:
+			if user_name == team.author:
+				teamNames.append(team.name)
+
+		return render_template('user_teams.html', teamNames=teamNames, name=current_user.username)
+	else:
+		return render_template('unauthorised_user.html')
+
+@app.route('/teams/')
+def teamsList():
+	teams = db.session.query(Team).all()
+	return render_template('teams.html', teams=teams), 200
+
+		
+@app.route('/teams/<team_name>/', methods=['GET', 'POST'])
+def teamDetails(team_name):
+	form = CommentForm()
+	comments = db.session.query(Comment).all()
+	teams = db.session.query(Team).all()
+	teamNames = []
+	
+	for team in teams:
+		teamNames.append(team.name)
+
+	if team_name in teamNames:
+		if form.validate_on_submit():
+			if current_user.is_authenticated:
+				newComment = Comment(text=form.text.data, author=current_user.username, teamName=team_name)
+				db.session.add(newComment)
+				db.session.commit()
+		
+				url = 'http://set09103.napier.ac.uk:9112/teams/' + team_name + '/' 	
+				return redirect(url)
+			else:
+				return redirect(url_for('login'))
+
+		return render_template('team_details.html', pokemon=pokemon, teams=teams, team_name=team_name, comments=comments, form=form)
+
+	else:
+		abort(404)
 
 @app.route('/pokedex/')
 def pokedex():
@@ -113,8 +196,7 @@ def login():
 				login_user(user, remember=form.remember.data)
 				return redirect(url_for('home'))		
 
-		return '<h1>Invalid username or password</h1>'
-		#return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+		return render_template('invalid.html')
 
 	return render_template('login.html', form=form)
 
@@ -134,26 +216,51 @@ def register():
 		db.session.add(newUser)
 		db.session.commit()		
 		
-		return '<h1>New user has been created!</h1>'
-		#return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+		return redirect(url_for('login'))
+	
 	return render_template('register.html', form=form)
 
-@app.route('/admin/', methods=['GET', 'POST'])
+@app.route('/admin/register/', methods=['GET', 'POST'], endpoint='adminregister')
+def registerAdmin():
+	form = RegisterAdminForm()
+
+	if current_user.administrator == True:
+		if form.validate_on_submit():
+			hashed_password = generate_password_hash(form.password.data, method='sha256')
+			newUser = User(username=form.username.data, email=form.email.data, password=hashed_password, administrator=form.administrator.data)
+			db.session.add(newUser)
+			db.session.commit()
+
+			return render_template('new_admin.html')
+
+		return render_template('register_admin.html', form=form)
+	
+	else:
+		return render_template('admins_only.html')
+
+@app.route('/admin/upload/', methods=['GET', 'POST'], endpoint='adminupload')
 def admin():
 	form = PokemonForm()
-
-	if form.validate_on_submit():
-		newPokemon = Pokemon(id=form.id.data, name=form.name.data, type1=form.type1.data, type2=form.type2.data, evolution=form.evolution.data, ability=form.ability.data, hiddenAbility=form.hiddenAbility.data, pokedexEntry=form.pokedexEntry.data)
+	
+	if current_user.administrator == True:
+		if form.validate_on_submit():
+			newPokemon = Pokemon(id=form.id.data, name=form.name.data, type1=form.type1.data, type2=form.type2.data, evolution=form.evolution.data, ability=form.ability.data, hiddenAbility=form.hiddenAbility.data, pokedexEntry=form.pokedexEntry.data)
 		
-		db.session.add(newPokemon)
-		db.session.commit()
+			db.session.add(newPokemon)
+			db.session.commit()
+			
+			url='http://set09103.napier.ac.uk:9112/pokedex/' + form.name.data + '/'
 
-		return '<h1>Pokemon has been added!</h1>'
-	return render_template('admin.html', form=form)
+			return redirect(url)
 
+		return render_template('upload.html', form=form)
+
+	else:
+		return render_template('admins_only.html')
 
 pokemonImgDivId1 = None
 pokemonImgDivId2 = None
+pokemonButtonLabel = None
 
 @app.route('/background_process_2/')
 def background_process_2():
@@ -162,8 +269,10 @@ def background_process_2():
 
 		pokemonImgDivId1 = '#' + pokemonButtonId + 'Div'
 		pokemonImgDivId2 = 'img#' + pokemonButtonId + 'Div > img'
+		pokemonButtonLabel = '#' + pokemonButtonId + 'Label'
+		teamFormHiddenId = '#' + pokemonButtonId + 'name'
 		
-		return jsonify(pokemonImgDivId2=pokemonImgDivId2, pokemonImgDivId1=pokemonImgDivId1)
+		return jsonify(pokemonImgDivId2=pokemonImgDivId2, pokemonImgDivId1=pokemonImgDivId1, pokemonButtonLabel=pokemonButtonLabel, teamFormHiddenId=teamFormHiddenId)
 
 	except Exception as e:
 		return str(e) 
@@ -181,14 +290,16 @@ def background_process():
 
 		if pokemonName in pokemonNames:
 			pokemonImagePath = '/static/images/gifs/' + pokemonName + '.gif'
-			return jsonify(result=pokemonName, pokemonImagePath=pokemonImagePath, pokemonImgDivId1=pokemonImgDivId1, pokemonImgDivId2=pokemonImgDivId2)
+			return jsonify(result=pokemonName, pokemonImagePath=pokemonImagePath, pokemonImgDivId1=pokemonImgDivId1, pokemonImgDivId2=pokemonImgDivId2, pokemonButtonLabel=pokemonButtonLabel)
 		else:
 			return jsonify(result=pokemonName + ' does not exist.')
 
 	except Exception as e:
 		return str(e)
 
-		
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('404.html'), 404
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
